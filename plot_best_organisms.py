ee8537027b11a1525ad4b7f1bb8f01a4734c994f
biertje72 <biertje72@gmail.com>
2021-05-02 23:55:05 +0200

    mean plotting

--- plot_best_organisms.py
+++ plot_best_organisms.py
@@ -1,11 +1,20 @@
 import os
 from pathlib import Path
 import matplotlib.pyplot as plt
+import numpy
 
-dir_path = "C:/AzureSandbox/MY_STUFF/NeuralEvolution/organisms"
+def running_mean(x, N):
+    cumsum = numpy.cumsum(numpy.insert(x, 0, 0)) 
+    return (cumsum[N:] - cumsum[:-N]) / float(N)
+
+dir_path = "./organisms10x32x4"
 paths = sorted(Path(dir_path).iterdir(), key=os.path.getmtime)
-fitnesses = [int(str(path).split("fit",1)[1][:3]) for path in paths]
-    
-plt.plot(fitnesses)
-plt.ylabel('some numbers')
+#fitnesses = [float(str(path).split("fit",1)[1][:4]) for path in paths]
+splits1 = [str(path).split("fit",1)[1] for path in paths]
+fitnesses = [float(split1.split("_",1)[0]) for split1 in splits1]
+fitnesses2 = running_mean(fitnesses, 15)
+#plt.plot(fitnesses)
+#plt.ylabel('some numbers')
+plt.plot(fitnesses2)
+plt.ylabel('running mean')
 plt.show()
